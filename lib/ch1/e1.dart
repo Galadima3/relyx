dc8ff39c18589fbd15ae34b9f88e87a84939ae4d
Galadima3 <abrahamgaladima300@gmail.com>
2024-04-19 06:54:00 +0100

    first commit

---
+++ lib/ch1/e1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Galadima3X';
+  String s1 = "Hello I am \"John Doe\"";
+  String s2 = 'Hello I\'m "John Doe"';
+  const int age = 7;
+  print("Name - $name");
+  print(s1);
+  print(s2);
+}
---
+++ test/relyx_test.dart
@@ -0,0 +1,8 @@
+import 'package:relyx/relyx.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
